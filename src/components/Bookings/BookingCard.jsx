4f63060e8ffb677e36f5c3393d8242ec74d59541
Ashwin <ashwin.m@bfhl.in>
2022-10-04 00:48:54 +0530

    fix: Fixed UI changes v1

--- src/components/Bookings/BookingCard.jsx
+++ src/components/Bookings/BookingCard.jsx
@@ -6,37 +6,49 @@ function BookingCard(props) {
     var name="Arjun";
 
     return (
-    <div id='bookingCard'>
-        <div className='turfCardContent'>
-            <img className='turfCardLogo' src="images\el_classico_logo.png" alt="" />
-            <div className='lhs'>
-                <h6>{props.location}</h6>
-                <div className='sportContainer'>
-                    <img src="images\football.png" alt="football" />
-                    <img src="images\cricket.png" alt="cricket" />
-                </div>
+      <div id="bookingCard">
+        <div className="priceImageStyleStyle">
+          <img
+            className="turfCardLogo"
+            src="images\el_classico_logo.png"
+            alt=""
+          />
+          <div className="lhs">
+            <h6>{props.location}</h6>
+            <div className="sportContainer">
+              <img src="images\football.png" alt="football" />
+              <img src="images\cricket.png" alt="cricket" />
             </div>
-            <div className='rhs bookingRHS'>
-                <div className='timings'>
-                    <img src="images\clock 1.svg" alt="clock" />
-                    <p>{props.startTime} - {props.endTime}</p>
-                </div>
-                <div className='timings'>
-                    <img src="images\clock 1.svg" alt="clock" />
-                    <p>{props.date}</p>
-                </div>
-                <div className='price'>
-                <img src="images\₹.svg" alt="clock" />
-                    <p>{props.price}</p>
-                </div>
+          </div>
+          <div className="rhs bookingRHS">
+            <div className="timings">
+              <img src="images\clock 1.svg" alt="clock" />
+              <p>
+                {props.startTime} - {props.endTime}
+              </p>
             </div>
+            <div className="timings">
+              <img src="images\clock 1.svg" alt="clock" />
+              <p>{props.date}</p>
+            </div>
+            <div className="price">
+              <img src="images\₹.svg" alt="clock" />
+              <p>{props.price}</p>
+            </div>
+          </div>
         </div>
-        <div className='cancelContainer'>
-            <div className='cancel' onClick={()=>{props.cancelModal(props)}}>
-                Click here to cancel the booking
-            </div>
+        <div className="cancelContainer">
+          <div
+            className="cancel"
+            onClick={() => {
+              props.cancelModal(props);
+            }}
+          >
+            Click here to cancel the booking
+          </div>
         </div>
-    </div> );
+      </div>
+    );
 }
 
 export default BookingCard;
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,17 +1,16 @@
-import TurfCard from '../TurfCard/TurfCard';
-import './LandingPage.css';
-import $ from 'jquery';
+import TurfCard from "../TurfCard/TurfCard";
+import "./LandingPage.css";
+import $ from "jquery";
 import { Link } from "react-router-dom";
-import { useEffect } from 'react';
-import { useState } from 'react';
-import React from 'react';
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-
+import { useEffect } from "react";
+import { useState } from "react";
+import React from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 function LandingPage(props) {
-  var name = 'arjun'
+  var name = "Arjun";
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [venues, setVenues] = useState([]);
@@ -21,10 +20,10 @@ function LandingPage(props) {
   // similar to componentDidMount()
   useEffect(() => {
     fetch("https://elclasico-test.herokuapp.com/venues")
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
-          console.log(result.data[0]._id)
+          console.log(result.data[0]._id);
           setIsLoaded(true);
           setVenues(result.data);
         },
@@ -35,47 +34,88 @@ function LandingPage(props) {
           setIsLoaded(true);
           setError(error);
         }
-      )
-  }, [])
-
+      );
+  }, []);
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <lottie-player src="images/Loader.json" background="transparent" speed="1" style={{ width: '300px', height: '100%' }} loop autoplay></lottie-player>
+    return (
+      <lottie-player
+        src="images/Loader.json"
+        background="transparent"
+        speed="1"
+        style={{ width: "300px", height: "100%" }}
+        loop
+        autoplay
+      ></lottie-player>
+    );
   } else {
     return (
       <div id="LandingPage" className="">
-        <div className='greeting'>
-          <h2>Hello <span className='userName'>{name}</span></h2>
+        <div className="greeting">
+          <h2>
+            Hey <span className="userName">{name + "!"}</span>
+          </h2>
+          <h6 className="subTitleText">What are you in mood for today?</h6>
         </div>
-        <div className='banner'>
-          <OwlCarousel className='owl-theme' items={1} stagePadding={200} center autoplay autoplayTimeout={3000} mouseDrag touchDrag dots={false} loop margin={10}>
-            <div class='item'>
+        <div className="banner">
+          <OwlCarousel
+            className="owl-theme"
+            items={1}
+            stagePadding={200}
+            center
+            autoplay
+            autoplayTimeout={3000}
+            mouseDrag
+            touchDrag
+            dots={false}
+            loop
+            margin={10}
+          >
+            <div class="item">
               <img src="images/carousel1.jpg" alt="" />
             </div>
-            <div class='item'>
+            <div class="item">
               <img src="images/carousel2.jpg" alt="" />
             </div>
-            <div class='item'>
+            <div class="item">
               <img src="images/carousel3.jpg" alt="" />
             </div>
-            <div class='item'>
+            <div class="item">
               <img src="images/carousel4.jpg" alt="" />
             </div>
           </OwlCarousel>
         </div>
-        <div className='greeting'>
-          <h4>Choose your turf</h4>
+        <div className="greeting">
+          <h4>Venues</h4>
         </div>
-        <div className='turfCardContainer'>
-          {venues.map(venue => (
-            <Link className="TurfCard" to="/bookingPreference" state={{ locality: venue.name, venue_id: venue._id, discount: 100 }} >
-              <TurfCard venue_id={venue._id} location={venue.name} startTime="6" endTime="11" startPrice="700" endPrice="800" discount="block" discountPrice="100" />
+        <div className="turfCardContainer">
+          {venues.map((venue) => (
+            <Link
+              className="TurfCard"
+              to="/bookingPreference"
+              state={{
+                locality: venue.name,
+                venue_id: venue._id,
+                discount: 100,
+              }}
+            >
+              <TurfCard
+                venue_id={venue._id}
+                location={venue.name}
+                startTime="6"
+                endTime="11"
+                startPrice="700"
+                endPrice="800"
+                discount="block"
+                discountPrice="100"
+              />
             </Link>
           ))}
         </div>
-      </div>);
+      </div>
+    );
   }
 }
 
